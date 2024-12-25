9b92c961c5465aa4257b5b71b2c72748c1d34bc6
Himanshu Kumar <himankshu1@gmail.com>
2024-12-25 23:20:03 +0530

    add db connection

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,13 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+
+//* IIFE for database connection
+(async function () {
+    await mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string);
+    console.log("connected to database");
+})();
 
 const app = express();
 
