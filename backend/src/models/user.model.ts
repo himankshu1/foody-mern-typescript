f6c90a3207a9e77863be3777fab618d0aed9699a
Himanshu Kumar <himankshu1@gmail.com>
2024-12-27 01:05:05 +0530

    add user schema

---
+++ backend/src/models/user.model.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        auth0Id: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+        name: {
+            type: String,
+        },
+        addressLine1: {
+            type: String,
+        },
+        city: {
+            type: String,
+        },
+        country: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+export const User = mongoose.model("User", userSchema);
